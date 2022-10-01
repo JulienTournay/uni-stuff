9613023176893a827505e08c031712573aa6e1fe
julientourany <jto@snowcrash.fritz.box>
2022-10-01 21:27:48 +0200

    First commit: Functions I've been using in my first two semesters repetedly. Clean-up and commentation will follow.

---
+++ README.md
@@ -0,0 +1,4 @@
+# MATH-STUFF
+
+Just some stuff I coded for university. I thought it would be cool to share it on Github; might even come in handy for someone.
+Mainly Latex-relatetd stuff and some calculations I'm too lazy to do by hand.
---
+++ math_stuff.py
@@ -0,0 +1,92 @@
+import numpy as num
+import sympy as sym
+import statistics as stats
+
+tab = '    '
+begin = '{'
+end = '}'
+number = '\\num{'
+
+
+def standardabweichung(list): return num.sqrt(num.sum([(i-stats.mean(list))**2 for i in list])/(len(list)-1))
+
+
+def get_data_csv(csv_path): return [[line[i] for line in num.recfromcsv(csv_path, delimiter=',')] for i in range(len(num.recfromcsv(csv_path, delimiter=',')[1]))]
+
+
+def get_mean_csv(csv_path): return [stats.mean(i) for i in [[line[i] for line in num.recfromcsv(csv_path, delimiter=',')] for i in range(len(num.recfromcsv(csv_path, delimiter=',')[1]))]]
+
+
+def get_mean(data): return [stats.mean(i) for i in data]
+
+
+def make_table_csv(csv_path):
+    data = get_data_csv(csv_path)
+    name = csv_path.split('/')[-1].split('.')[0]
+    head = f'\n{tab}'.join([
+        '\\begin{table}[]',
+        '\\centering',
+        '\\begin{tabular}{|' + 'c|'*len(data) + '}',
+        tab + '\\hline'
+        ])
+    foot = tab*2 + f'\n{tab}'.join([
+        '\\hline',
+        '\\end{tabular}',
+        '\\caption{' + name + '}',
+        '\\label{tab:' + name + '}\n\\end{table}'
+        ])
+    print(head)
+    for n in range(len(data[0])):
+        cont = f'{tab*2}'
+        first = True
+        for item in data:
+            if first:
+                cont += f'{number}{item[n]}{end}'
+                first = False
+            else: cont += f' & {number}{item[n]}{end}'
+        cont += ' \\\\'
+        print(cont)
+    print(foot)
+
+
+def make_table(data):
+    print('\\begin{table}[]\n    \\centering\n    \\begin{tabular}{|' + ''.join(['c|' for item in data]) + '}\n        \\hline')
+    for n in range(len(data[0])):
+        cont = f'{tab*2}'
+        first = True
+        for item in data:
+            if first:
+                if isinstance(item[n], num.number): cont += f'{number}{item[n]:3.2f}{end}'
+                elif isinstance(item[n], sym.Float): cont += f'{number}{item[n]:3.2f}{end}'
+                else: cont += f'{item[n]}'
+                first = False
+            else:
+                if isinstance(item[n], num.number): cont += f' & {number}{item[n]:3.2f}{end}'
+                elif isinstance(item[n], sym.Float): cont += f' & {number}{item[n]:3.2f}{end}'
+                else: cont += f' & {item[n]}'
+        cont += ' \\\\'
+        print(cont)
+    print("        \\hline\n    \\end{tabular}\n    \\caption{Caption}\n    \\label{tab:my_label}\n\\end{table}")
+
+
+def gauss(formula, vars, space=1, other_vars=[]):
+    new_stack = []
+    for n in range(space):
+        list_formeln = []
+        for i in range(len(vars)):
+            derivative = formula.diff(vars[i][0])
+            for j in vars: derivative = derivative.subs(j[0], j[1][n])
+            for k in other_vars: derivative = derivative.subs(k[0], k[1])
+            list_formeln.append((derivative * vars[i][2])**2)
+        new_stack.append(sym.Float(sym.sqrt(sum(list_formeln))))
+    return new_stack
+
+
+def gauss_latex(formula, vars):
+    list_formulas = []
+    for i in range(len(vars)):
+        derivative = formula.diff(vars[i][0])
+        list_formulas.append(f'&+ \\left({sym.latex(derivative)} \\Delta {sym.latex(vars[i][0])} \\right)^2 \\nonumber \\\\')
+    print('\\begin{align}')
+    for n in range(len(list_formulas)): print(tab + list_formulas[n])
+    print(tab + '\\label{gaussformel}\n\\end{align}')
